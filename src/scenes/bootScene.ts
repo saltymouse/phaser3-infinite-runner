1892866ef8e82a365034be30e58d74dbd39940a2
saltymouse <saltymouse@outlook.com>
2018-10-17 18:47:20 +0900

    temporarily dropped bootScene (stright to preload)... might use it later

--- src/scenes/bootScene.ts
+++ src/scenes/bootScene.ts
@@ -7,24 +7,20 @@ export class BootScene extends Phaser.Scene {
     super({ key: "BootScene" });
   }
 
-  // Comment toggle init() on/off for debugging
+  // Comment toggle this init() on/off for debugging
   init(): void { console.log(this.scene.key) }
 
   preload(): void {
     // load all files necessary for the loading screen
 
-    // load out our json asset package
+    // load our json asset pack(age)
     // https://photonstorm.github.io/phaser3-docs/Phaser.Loader.LoaderPlugin.html#pack
-    this.load.pack(
-      "preload",
-      "./src/assets/pack.json",
-      "preload"
-    );
+    this.load.json("assets", "./src/assets/pack.json");
     this.load.image('logo', './src/assets/sprites/Enemies/flyMan_fly.png');
   }
 
   create(): void {
-    // now that we've got our json file preloaded, we can continue on to the next
+    // now that we've got our json file loaded, we can continue on to the next
     // scene to actually load the files specified within above json file!!! (?)
     this.scene.start('PreloadScene')
   }
